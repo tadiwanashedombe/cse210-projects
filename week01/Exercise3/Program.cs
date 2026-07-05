b1c9a9fdd6be6265009b50905f1c7400874beaa7
tadiwanashedombe <dombeseant@gmail.com>
2026-07-05 03:20:58 +0200

    loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,11);
+        
+        int guess = 0;
+        while(guess != number){
+            Console.Write("What is the magic number ? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if(guess == number)
+            {
+                Console.WriteLine("You guessed it !");
+            }else if(guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+                
+            }
+        }
     }
 }
