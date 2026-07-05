3c6d84feca7b09af7f89c9d7f519b18d625a4582
tadiwanashedombe <dombeseant@gmail.com>
2026-07-05 02:59:15 +0200

    if statements

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is you first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What is you last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {last_name}, {first_name} {last_name}.");
+
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        string grade = "invalid";
+
+        Console.Write("Enter mark percentage: ");
+        int mark = int.Parse(Console.ReadLine());
+        
+        
+        if(mark < 0 && mark < 100){
+            Console.WriteLine("Percentage can not be below 0!");
+        }
+        else if(mark >= 90)
+        {
+            grade = "A";
+        }else if(mark >= 80){
+            grade = "B";
+        }else if(mark >= 70){
+            grade = "C";
+        }else if(mark >= 60){
+            grade = "D";
+        }else if(mark < 60){
+            grade = "F";
+        }
+
+        // pass 
+        if(mark >= 70)
+        {
+            Console.WriteLine("Congratulations, You passed this course");
+        }else
+        {
+            Console.WriteLine("Failed, Better luck next time.");
+        }
+        
+        Console.WriteLine($"Grade :{grade}");
     }
 }
