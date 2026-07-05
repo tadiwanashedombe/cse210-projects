79cc1cd8d97806eba9ee1f05340fd1549a309a33
tadiwanashedombe <dombeseant@gmail.com>
2026-07-05 04:35:38 +0200

    functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        static void DisplayWelcome()
+        {
+                   Console.WriteLine("Welcome to the program!");
+        }
+
+        DisplayWelcome();
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name : ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+        string name  = PromptUserName();
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number : ");
+            int number = int.Parse(Console.ReadLine());
+
+            return number;
+        }
+        int number = PromptUserNumber();
+
+        static int SquareNumber(int num)
+        {
+            int square  = num*num;
+
+            return square;
+        }
+        int square = SquareNumber(number);
+
+        static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
+
+        DisplayResult(name, square);
+
     }
 }
