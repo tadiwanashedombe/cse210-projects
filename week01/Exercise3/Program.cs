7d83de7e3d41ec82022a93782e3ccd39ab5968d0
tadiwanashedombe <dombeseant@gmail.com>
2026-07-05 03:24:57 +0200

    added guess count

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -8,13 +8,18 @@ class Program
         int number = randomGenerator.Next(1,11);
         
         int guess = 0;
+        int guess_count = 0;
+
         while(guess != number){
             Console.Write("What is the magic number ? ");
             guess = int.Parse(Console.ReadLine());
 
+            guess_count++;
+
             if(guess == number)
             {
-                Console.WriteLine("You guessed it !");
+                Console.WriteLine($"You guessed it !\nYou took {guess_count} guesses.");
+
             }else if(guess > number)
             {
                 Console.WriteLine("Lower");
