e8d1627c73491c441fa9adf6309005a6bddec75e
tadiwanashedombe <dombeseant@gmail.com>
2026-07-07 14:07:59 +0200

    fixed exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,31 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1,11);
+        // For Parts 1 and 2, where the user specified the number...
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
         
-        int guess = 0;
-        int guess_count = 0;
+        // For Part 3, where we use a random number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
 
-        while(guess != number){
-            Console.Write("What is the magic number ? ");
+        int guess = -1;
+
+        // We could also use a do-while loop here...
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
 
-            guess_count++;
-
-            if(guess == number)
+            if (magicNumber > guess)
             {
-                Console.WriteLine($"You guessed it !\nYou took {guess_count} guesses.");
-
-            }else if(guess > number)
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
             {
                 Console.WriteLine("Lower");
             }
             else
             {
-                Console.WriteLine("Higher");
-                
+                Console.WriteLine("You guessed it!");
             }
-        }
+
+        }                    
     }
 }
