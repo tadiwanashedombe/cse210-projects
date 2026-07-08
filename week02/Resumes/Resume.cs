a2c175341709201d7dad89dfc26d655464009888
tadiwanashedombe <dombeseant@gmail.com>
2026-07-08 11:37:53 +0200

    abstraction

---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}\nJobs:");
+        foreach(Job j in _jobs)
+        {
+            j.Display();
+        }
+    }
+}
