83e5f9e2df68eabe76f3ea9b67360a5ca9f2d0e5
tadiwanashedombe <dombeseant@gmail.com>
2026-07-15 08:52:30 +0200

    learnign activity  complete

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction(1);
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3,4);
+        Fraction f4 = new Fraction(1,3);
+
+        Console.WriteLine($"{f1.GetFractionString()}\n{f1.GetDecimalValue()}");
+        Console.WriteLine($"{f2.GetFractionString()}\n{f2.GetDecimalValue()}");
+        Console.WriteLine($"{f3.GetFractionString()}\n{f3.GetDecimalValue()}");
+        Console.WriteLine($"{f4.GetFractionString()}\n{f4.GetDecimalValue()}");
+
+
+        
+
     }
 }
